1df008248010fa610ec60225764c31483f0a90ee
Assad <astb01@gmail.com>
2021-10-06 16:38:27 +0100

    Weather App

--- session-8/challenges/weather-app/weather.js
+++ session-8/challenges/weather-app/weather.js
@@ -1,8 +1,9 @@
-const weatherAPIKey = ``;
+import { WEATHER_API_KEY } from "./app-key.js";
+
+const weatherAPIKey = WEATHER_API_KEY;
 const weatherAPIUrl = ` http://api.weatherapi.com/v1/current.json?key=${weatherAPIKey}`;
 
 // DOM Elements:
-const weatherResult = document.querySelector(".weather-result");
 const temperatureHeading = document.getElementById("temperature");
 const locationNameHeading = document.getElementById("locationName");
 const submitBtn = document.querySelector(".submit-btn");
